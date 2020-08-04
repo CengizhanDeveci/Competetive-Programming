8d6cf9771ea6972157eec60d2865cdd0ecdb37cb
CengizhanDeveci <devecicengizhan01@gmail.com>
2020-08-04 11:31:09 +0300

    Done

---
+++ algorithms/1534. Count Good Triplets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        count = 0
+        for i in range(len(arr)):
+            for j in range(1,len(arr)):
+                for k in range(2,len(arr)):
+                    if i < j < k and abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        count += 1
+    
+        return count
+        
