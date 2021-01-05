d624ed83bbbfefa0f052d91dfe3e51269c009175
CengizhanDeveci <devecicengizhan01@gmail.com>
2021-01-05 15:20:51 +0300

    100. Same Tree.py added

---
+++ algorithms/100. Same Tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p == None and q == None:
+            return True
+        if (p == None and q != None) or (p != None and q == None):
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
