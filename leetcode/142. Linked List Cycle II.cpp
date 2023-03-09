9c54fb445fe2b4690338bd4aabf7c4777b59fa4b
CengizhanDeveci <devecicengizhan01@gmail.com>
2023-03-10 02:10:51 +0300

    142. Linked List Cycle II.cpp added

--- leetcode/142. Linked List Cycle II.cpp
+++ leetcode/142. Linked List Cycle II.cpp
@@ -11,6 +11,26 @@ struct ListNode {
  
 class Solution {
 public:
+    // with floyd cycle algorithm
+    ListNode *detectCycle2(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+
+    // O(n) memory complexity
     ListNode *detectCycle(ListNode *head) {
         unordered_set<ListNode *> visited;
         if(!head) return NULL;
